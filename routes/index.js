2e778a53192c33a7658ce5b490f76b1ceee13051
BigJonas <jonasgaribay012@gmail.com>
2023-11-12 17:04:07 -0600

    Added basic log in page and dashboard page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.post('/submit', function(req, res, next) {
+  const { username, password} = req.body;
+  console.log(username + ", " + password);
+
+  res.render('dashboard');
 });
 
 module.exports = router;
